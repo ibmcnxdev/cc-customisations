f4a0847e505e0156905649ac619463b36dc198d3
Jay Agrawal <jagrawal@isw.net.au>
2017-10-04 13:08:49 +1000

    Update cc-utils.js

--- core/cc-utils.js
+++ core/cc-utils.js
@@ -1,18 +1,25 @@
-var ccUtils = {
-  loadWhenReady: function(initFn, conditionalFns, ignoreHashChange) {
+console.log("core loaded");
+var iswExtensions = {
+  loadWhenReady: function(initFn, conditionalFns, ignoreHashChange){
+
     if(typeof initFn !=='function' || !Array.isArray(conditionalFns)) {
       return console.error('Invalid Params', arguments);
     }
+
     var MAX_ATTEMPTS = 30;
+
     var numAttemptsForDojo = 0;
     var mapLoadEvent = function(){
-      if(dojo){
+      if(typeof dojo=="object"){
         if(!ignoreHashChange) dojo.addOnLoad(mapHashChangeEvent);
         dojo.addOnLoad(fnCallOnLoad);
       }
       else if(numAttemptsForDojo++ <= MAX_ATTEMPTS)
       setTimeout(mapLoadEvent, 1000);
     };
+
+
+
     var currentHref = window.location.href;
     var mapHashChangeEvent = function(){
       //map method to onHashChange event
@@ -33,9 +40,11 @@ var ccUtils = {
         }, 1000);
       }
     };
+
     var numAttempts = {};
-    var fnCallOnLoad = function() {
-      try {
+
+    var fnCallOnLoad = function(){
+      try{
         var val;
         for(var i=0; i<conditionalFns.length; i++) {
           var fn = conditionalFns[i];
@@ -51,16 +60,22 @@ var ccUtils = {
         console.error('fnCallOnLoad: ', e);
       }
     };
+
     mapLoadEvent();
   },
-  fireClickEvent: function(el) {
+  fireClickEvent: function(el){
     if(document.createEvent) {
       var event = document.createEvent('MouseEvents');
       event.initEvent( 'click', true, false );
       el.dispatchEvent( event );
+
     } else if(document.createEventObject && el.fireEvent) {
       var event = document.createEventObject();
       el.fireEvent( 'onclick', event );
     }
+  },
+  addCssStyle:function(styleStr){
+	var head = document.head || document.getElementsByTagName('head')[0];
+	if(styleStr && head) dojo.create('style', {innerHTML:styleStr}, head);
   }
 };
