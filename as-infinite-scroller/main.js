554413906f3e46a253527e682c1510143ab20614
Jay Agrawal <jagrawal@isw.net.au>
2017-10-04 13:12:54 +1000

    Update and rename as-infinite-scroller.js to main.js

--- as-infinite-scroller/main.js
+++ as-infinite-scroller/main.js
@@ -1,4 +1,15 @@
+/**
+* @author: jagrawal@isw.net.au
+* @date: 16/06/2015
+**/
+
+console.log('scroller loaded');
+
 (function(){
+var showMoreBtnClickDelay = 500;
+var showMoreDomQuery = 'ul.lotusStream li.pagingHandler a, ul.lotusPagingBottom li.pagingHandler a.streamMore';
+var infiniteScrollThreshold = 700;
+
   var scrollEvent,scrollTimeout = false, scrollCount=0,intervalCount=0 ;
   var mapInfiniteScrollEvent = function(){
     if(scrollEvent) dojo.disconnect(scrollEvent);
@@ -8,6 +19,7 @@
       scrollTimeout = setTimeout(scrollFunc, 200);
     });
   };
+
   var lastClick = new Date();
   var scrollFunc = function(){
     // console.log('called scrollfunc', intervalCount++);
@@ -15,18 +27,19 @@
     var documentHeight = getDocHeight();
     var windowHeight = window.innerHeight;
     // console.log('scrolling window', scrollPos, documentHeight, windowHeight);
-    if(scrollPos >= documentHeight - windowHeight - ncConfig.infiniteScrollThreshold){
+    if(scrollPos >= documentHeight - windowHeight - infiniteScrollThreshold){
       var thisClick = (new Date()).getTime();
-      if(thisClick - lastClick < ncConfig.showMoreBtnClickDelay) return;
-      var showMoreBtn = dojo.query(ncConfig.domQueries.showMoreBtn);
-      if(showMoreBtn && showMoreBtn.length==1 && dojo.style(showMoreBtn[0],'display'!=='none')){
+      if(thisClick - lastClick < showMoreBtnClickDelay) return;
+      var showMoreBtn = dojo.query(showMoreDomQuery);
+      if(showMoreBtn && showMoreBtn.length>0 && dojo.style(showMoreBtn[0],'display'!=='none')){
         lastClick = thisClick;
-        iswTweaks.fireClickEvent(showMoreBtn[0]);
+        iswExtensions.fireClickEvent(showMoreBtn[0]);
       }
       else
-        console.error('InfiniteScroller: No Show More btn found', ncConfig.domQueries.showMoreBtn);
+        console.error('InfiniteScroller: No Show More btn found', showMoreDomQuery);
     }
   };
+
   function getDocHeight() {
     var D = document;
     return Math.max(
@@ -35,6 +48,7 @@
       Math.max(D.body.clientHeight, D.documentElement.clientHeight)
     );
   }
+
   function getScrollTop(){
     if(typeof pageYOffset!= 'undefined'){
         //most browsers except IE before #9
@@ -47,11 +61,12 @@
         return D.scrollTop;
     }
   }
-  if(iswTweaks) {
-    iswTweaks.loadWhenReady(mapInfiniteScrollEvent, [
+  if(iswExtensions) {
+	  iswExtensions.loadWhenReady(mapInfiniteScrollEvent, [
       function(){
         return ibmConfig && ibmConfig.serviceName==='homepage' && window.location.pathname.indexOf('updates')>-1;
       }
     ]);
-  } else console.error('missing dependency - iswTweaks');
+  } else console.error('missing dependency - iswExtensions');
+
 })();
